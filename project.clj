91a4abfcb86c1f1178ca5ca093d79e2d445cf62a
zcaudate <z@caudate.me>
2012-10-17 14:01:57 +1100

    Bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sigmund "0.1.0"
+(defproject sigmund "0.1.1"
   :description "System analytics for Clojure"
   :url "https://github.com/zcaudate/sigmund"
   :license {:name "Eclipse Public License"
