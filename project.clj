5f7447e2d07101b5dea1b5d93160f33460377f90
Chris Zheng <z@caudate.me>
2013-10-27 21:14:10 +1100

    temporary solution for jre 7 on osx 10.8.5

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject sigmund "0.1.1"
+(defproject im.chit/sigmund "0.2.0"
   :description "System analytics for Clojure"
   :url "https://github.com/zcaudate/sigmund"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.fusesource/sigar "1.6.4"]
-                 [sigar/sigar-native-deps "1.6.4"]]
-
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]}})
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [sigar/sigar "1.7.0-v20131027"]
+                 [sigar/sigar-native-deps "1.7.0-v20131027"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
